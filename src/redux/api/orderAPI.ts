a5e51d12b16ed648c9944528e66986a48f671cc5
Yash Mishra <yashmishra06112310@gmail.com>
2024-07-21 14:55:32 +0530

    Commited Changes

--- src/redux/api/orderAPI.ts
+++ src/redux/api/orderAPI.ts
@@ -1,48 +1,63 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { AllOrdersResponse, MessageResponse, OrderDetailsResponse, UpdateOrderRequest ,} from "../../types/api-types";
-import { OrderItem } from "../../types/types";
+import {
+  AllOrdersResponse,
+  MessageResponse,
+  NewOrderRequest,
+  OrderDetailsResponse,
+  UpdateOrderRequest,
+} from "../../types/api-types";
 
-
-export const  orderApi=createApi({
-    reducerPath:"orderApi",
-    baseQuery: fetchBaseQuery({
-        baseUrl: `${import.meta.env.VITE_SERVER}/api/v1/order/`,
+export const orderApi = createApi({
+  reducerPath: "orderApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${import.meta.env.VITE_SERVER}/api/v1/order/`,
+  }),
+  tagTypes: ["orders"],
+  endpoints: (builder) => ({
+    newOrder: builder.mutation<MessageResponse, NewOrderRequest>({
+      query: (order) => ({
+        url: "new",
+        method: "POST",
+        body: order,
       }),
-      tagTypes:["orders"],
-      endpoints:(builder)=>({
-        newOrder:builder.mutation<MessageResponse,OrderItem>({
-            query:(order)=>({url:"new",method:"POST",body:order}),
-           // invalidatesTags=["orders"],
-        }),
-        updateOrder:builder.mutation<MessageResponse,UpdateOrderRequest>({
-            query:({userId,orderId})=>({
-                url:`${orderId}?id=${userId}`,
-                method:"PUT",
-            })
-        }),
-        deleteOrder:builder.mutation<MessageResponse,UpdateOrderRequest>({
-            query:({userId,orderId})=>({
-                url:`${orderId}?id=${userId}`,
-                method:"DELETE",
-            })
-        }),
-        myOrders:builder.query<AllOrdersResponse,string>({
-            query:(id)=>`my?id=${id}`
-        }),
-        allOrders:builder.query<AllOrdersResponse,string>({
-            query:(id)=>`all?id=${id}`
-        }),
-        orderDetails:builder.query<OrderDetailsResponse,string>({
-            query:(id)=>id
-        }),
-      })
+      invalidatesTags: ["orders"],
+    }),
+    updateOrder: builder.mutation<MessageResponse, UpdateOrderRequest>({
+      query: ({ userId, orderId }) => ({
+        url: `${orderId}?id=${userId}`,
+        method: "PUT",
+      }),
+      invalidatesTags: ["orders"],
+    }),
+    deleteOrder: builder.mutation<MessageResponse, UpdateOrderRequest>({
+      query: ({ userId, orderId }) => ({
+        url: `${orderId}?id=${userId}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["orders"],
+    }),
+    myOrders: builder.query<AllOrdersResponse, string>({
+      query: (id) => `my?id=${id}`,
+      providesTags: ["orders"],
+    }),
+    allOrders: builder.query<AllOrdersResponse, string>({
+      query: (id) => `all?id=${id}`,
+      providesTags: ["orders"],
+    }),
+    orderDetails: builder.query<OrderDetailsResponse, string>({
+      query: (id) => id,
+      providesTags: ["orders"],
+    }),
+  }),
 });
 
 export const {
-    useNewOrderMutation,
-    useUpdateOrderMutation,
-    useDeleteOrderMutation,
-    useMyOrdersQuery,
-    useAllOrdersQuery,
-    useOrderDetailsQuery,
-  } = orderApi;
+  useNewOrderMutation,
+  useUpdateOrderMutation,
+  useDeleteOrderMutation,
+  useMyOrdersQuery,
+  useAllOrdersQuery,
+  useOrderDetailsQuery,
+} = orderApi;
+
+
